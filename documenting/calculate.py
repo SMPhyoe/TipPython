62ce27aedd88f138c265685e8c17a45e7d843622
SMPhyoe <sumyatphyoe.qqsm@gmail.com>
2023-11-09 11:16:18 +0800

    Example of Documentation Comments

---
+++ documenting/calculate.py
@@ -0,0 +1,18 @@
+# Created by Su Myat Phyoe at 10:53 am 9/11/2023 using PyCharm
+
+
+def calculate_total(price, quantity):
+    """
+    Calculate the total cost.
+
+    Args:
+        price (float): The price per unit.
+        quantity (int): The quantity of items.
+
+    Returns:
+        float: The total cost.
+    """
+    return price * quantity
+
+
+calculate_total()
